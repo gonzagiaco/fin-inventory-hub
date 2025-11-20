8060ad7219af66e978b8a1a15c617ce8d5bf49fe
JDnasello <jnasellopron@gmail.com>
2025-11-20 10:27:18 -0300

    Fix: Hamburguer button z index

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-44c91e4f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.eof7nsang1o"
+    "revision": "0.bludbir8jn8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
